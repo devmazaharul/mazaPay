5df42f80ae73156c46981b3a8860a2abbabdc9fd
mazaharul islam <mazaharul>
2025-07-28 21:33:23 +0600

    add

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const cors = require('cors');
 app.use(express.static('public'));
 app.use(
   cors({
-    origin: 'http://localhost:3000',
+    origin: 'http://57.181.173.193:3000',
     credentials: true,
   })
 );
